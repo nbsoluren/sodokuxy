d5134bcec91288cdb8399fe37c3baaa12d213b4a
ftsaylon <ftsaylon@up.edu.ph>
2018-05-03 21:55:43 +0800

    Edited sudoku.c and sudoku.h

--- sudoku.h
+++ sudoku.h
@@ -1,4 +1,19 @@
 // ---------------------------------------------------------------------------------- //
+// CONSTANTS
+#define BLANK 0
+// ---------------------------------------------------------------------------------- //
+
+// ---------------------------------------------------------------------------------- //
+// STRUCTURE
+typedef struct node{
+    int row;
+    int col;
+    int val;
+}NODE;
+// ---------------------------------------------------------------------------------- //
+
+
+// ---------------------------------------------------------------------------------- //
 // FUNCTIONS
 
 // Function that prints a Matrix
@@ -19,7 +34,7 @@ void destroy(int **board, int board_size){
   }
 }
 
-// Check if number is already used in the row
+// Checks if number is already used in the row
 bool usedInRow(int **board, int board_size, int row, int num){
 	int col;
 	for(col=0; col<board_size; col++){ //For-loop that traverses through the rows
@@ -28,7 +43,7 @@ bool usedInRow(int **board, int board_size, int row, int num){
 	return false;
 }
 
-// Check if number is already used in the column
+// Checks if number is already used in the column
 bool usedInCol(int **board, int board_size, int col, int num){
 	int row;
 	for(row=0; row<board_size; row++){ //For-loop that traverses through the columns
@@ -37,7 +52,7 @@ bool usedInCol(int **board, int board_size, int col, int num){
 	return false;
 }
 
-// Check if number is already used in the grid
+// Checks if number is already used in the grid
 bool usedInBox(int **board, int subgrid_size, int boxStartRow, int boxStartCol, int num){
 	int row, col;
 	for(row=0; row<subgrid_size; row++){ // Traverse through the grid
@@ -49,19 +64,51 @@ bool usedInBox(int **board, int subgrid_size, int boxStartRow, int boxStartCol, 
 	return false;
 }
 
+// Checks if number is safe to input to the board
 bool isSafe(int **board, int board_size, int subgrid_size, int row, int col, int num){
 	return !usedInRow(board, board_size, row, num) &&
 				 !usedInCol(board, board_size, col, num) &&
 				 !usedInBox(board, subgrid_size, row-row%subgrid_size, col-col%subgrid_size, num);
 }
 
+// Checks if the board is already complete
+int findBlank(int **board, int board_size){
+	int i, j, counter=0;
+	for(i=0; i<board_size; i++){
+		for(j=0; j<board_size; j++){
+			if(board[i][j] == BLANK) counter++;
+		}
+	}
+
+	return counter;
+}
+
 // Pop Function
-int pop(int stack[], int stack_size){
-	int i, val = stack[0]; // Get TOS
+void pop(NODE stack[], int stack_size){
+	int i, val = stack[0].val; // Get TOS
 	for(i=1; i<stack_size-1; i++){ // Move elements of the stack to the left
 		stack[i-1] = stack[i];
 	}
-	return val;
+}
+
+// Push Function
+void push(NODE stack[], int index, int num){
+	stack[index].val = num;
+}
+
+void fillUp(){
+
 }
 
 // ---------------------------------------------------------------------------------- //
+
+void printMatrix(NODE **matrix, int row, int col){
+	int i, j;
+
+	for(i=0; i<row; i++){
+		for(j=0; j<col; j++){
+			printf("%d ", (matrix[i][j]).val);
+		}
+		printf("\n");
+	}
+}
